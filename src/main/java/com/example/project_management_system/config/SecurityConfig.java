ca023aab530bfe2a0b91ead28e784ca2a6226180
Yağmur Çiçekdağı <ygmcicekdagi@gmail.com>
2026-02-21 13:22:11 +0300

    feat: add security config class

---
+++ src/main/java/com/example/project_management_system/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.project_management_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth.requestMatchers("/error").permitAll()
+            .requestMatchers("/api/employees/**").permitAll().anyRequest().authenticated())
+        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .httpBasic(Customizer.withDefaults());
+
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public UserDetailsService users() {
+    UserDetails user = User.builder().username("user")
+        .password(passwordEncoder().encode("password")).roles("USER").build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
+}
