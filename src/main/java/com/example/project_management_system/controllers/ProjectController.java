4ff5444b4c5f4367f580c522a370899c720725a2
Yağmur Çiçekdağı <ygmcicekdagi@gmail.com>
2026-02-21 13:40:05 +0300

    feat(project): implement controller layer

---
+++ src/main/java/com/example/project_management_system/controllers/ProjectController.java
@@ -0,0 +1,70 @@
+package com.example.project_management_system.controllers;
+
+import java.net.URI;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.project_management_system.dtos.ProjectCreateRequest;
+import com.example.project_management_system.dtos.ProjectResponse;
+import com.example.project_management_system.dtos.ProjectUpdateRequest;
+import com.example.project_management_system.services.ProjectService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@Validated
+@RequestMapping("/api/projects")
+public class ProjectController {
+
+  private final ProjectService projectService;
+
+  public ProjectController(ProjectService projectService) {
+    this.projectService = projectService;
+
+  }
+
+  @PostMapping
+  public ResponseEntity<ProjectResponse> create(@Valid @RequestBody ProjectCreateRequest req) {
+    ProjectResponse created = projectService.create(req);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(created.id()).toUri();
+    return ResponseEntity.created(location).body(created);
+  }
+
+  @PatchMapping("/{id}")
+  public ProjectResponse patch(@PathVariable Long id, @RequestBody ProjectUpdateRequest req) {
+    return projectService.patch(id, req);
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<ProjectResponse>> findAll(@PageableDefault Pageable pageable) {
+    Page<ProjectResponse> projects = projectService.findAll(pageable);
+    return ResponseEntity.ok(projects);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ProjectResponse> findById(@PathVariable Long id) {
+    Optional<ProjectResponse> res = projectService.findById(id);
+    return ResponseEntity.of(res);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    projectService.deleteById(id);
+    return ResponseEntity.noContent().build();
+  }
+}
