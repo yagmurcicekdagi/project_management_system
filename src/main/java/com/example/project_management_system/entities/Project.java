7f65044c075d14dc858cc17fae164cbd81cb36fa
Yağmur Çiçekdağı <ygmcicekdagi@gmail.com>
2026-02-24 21:22:05 +0300

    ref(project): use timestampz data type for createdAt and updatedAt columns

--- src/main/java/com/example/project_management_system/entities/Project.java
+++ src/main/java/com/example/project_management_system/entities/Project.java
@@ -3,6 +3,7 @@ package com.example.project_management_system.entities;
 import java.time.Instant;
 import java.time.LocalDate;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -29,32 +30,32 @@ import lombok.Setter;
 @Table(name = "projects")
 public class Project {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String name;
+    @Column(nullable = false)
+    private String name;
 
-  @Column(columnDefinition = "TEXT")
-  private String description;
+    @Column(columnDefinition = "TEXT")
+    private String description;
 
-  @Enumerated(EnumType.STRING)
-  @Column(nullable = false)
-  @Builder.Default
-  private ProjectStatus status = ProjectStatus.NEW;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @ColumnDefault("NEW")
+    private ProjectStatus status;
 
-  @Column(name = "start_date")
-  private LocalDate startDate;
+    @Column(name = "start_date")
+    private LocalDate startDate;
 
-  @Column(name = "end_date")
-  private LocalDate endDate;
+    @Column(name = "end_date")
+    private LocalDate endDate;
 
-  @CreationTimestamp
-  @Column(name = "created_at", nullable = false, updatable = false)
-  private Instant createdAt;
+    @CreationTimestamp
+    @Column(name = "created_at", columnDefinition = "timestamptz", nullable = false, updatable = false)
+    private Instant createdAt;
 
-  @UpdateTimestamp
-  @Column(name = "updated_at", nullable = false)
-  private Instant updatedAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at", columnDefinition = "timestamptz", nullable = false)
+    private Instant updatedAt;
 }
